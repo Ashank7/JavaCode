b022ee77cbca0795147ee568f878123dd42248dc
Ashank007 <guptaashank38@gmail.com>
2025-04-16 12:06:48 +0530

    Added Expection

--- Array/Main.java
+++ Array/Main.java
@@ -2,7 +2,7 @@ package Array;
 
 public class Main{
 	public static void main(String [] args) {
-		int arr[] = {12,14,15,17,19};		
+		int arr[] = {12,14,15,17,19};	
 		
 //		for(int i=0;i<arr.length;i++) {
 //			System.out.println(arr[i]);
---
+++ Exception/Main.java
@@ -0,0 +1,17 @@
+public class Main {
+    public static void logic1(){
+      System.out.println("Logic1 - Line 1");
+      logic2();
+      System.out.println("Logic2 - Line 2");
+   }
+    public static void logic2() {
+      try {
+      System.out.println(10/0);
+      } catch (Exception e) {
+      System.out.println(e);
+      }
+   }
+     public static void main(String [] args){
+        logic1();
+   }
+ } 
