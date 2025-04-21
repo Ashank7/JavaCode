96856244894a8199b3d0fd1f917664d274132ffa
Ashank007 <guptaashank38@gmail.com>
2025-04-21 12:55:37 +0530

    Writing Data into Text File

--- FileHandling/FileWrite.java
+++ FileHandling/FileWrite.java
@@ -3,6 +3,12 @@ import java.io.IOException;
 
 public class FileWrite {
 public static void main(String [] args) throws IOException{
-  FileWriter f = new FileWriter("abc.txt"); 
+  FileWriter f = new FileWriter("abc.txt");
+  f.write("Hello World");
+  f.write('\n');
+  f.write("Doing File Handling in Java");
+  f.flush();
+  f.close();
+  System.out.println("Done Writting");
   }
 }
