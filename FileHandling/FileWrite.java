5090e44a175c25e823e10f543cd381178724fc8a
Ashank007 <guptaashank38@gmail.com>
2025-04-21 12:46:22 +0530

    Added FileWriter Class

---
+++ FileHandling/FileWrite.java
@@ -0,0 +1,8 @@
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class FileWrite {
+public static void main(String [] args) throws IOException{
+  FileWriter f = new FileWriter("abc.txt"); 
+  }
+}
