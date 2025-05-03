d74c9789409fe8a1c31b7375396dddd20990fda0
Ashank007 <guptaashank38@gmail.com>
2025-05-03 12:27:32 +0530

    Added DiplomaCourse in exp7

--- Lab/exp7.java
+++ Lab/exp7.java
@@ -1,6 +1,6 @@
 class Course {
     String courseName;
-    int duration; 
+    int duration;
 
     public Course(String courseName, int duration) {
         this.courseName = courseName;
@@ -37,15 +37,33 @@ class PGCourse extends Course {
     }
 }
 
+class DiplomaCourse extends Course {
+    public DiplomaCourse(String courseName, int duration) {
+        super(courseName, duration);
+    }
+
+    @Override
+    public void displayDetails() {
+        System.out.println("Diploma Course:");
+        super.displayDetails();
+    }
+}
+
 public class exp7 {
     public static void main(String[] args) {
         Course ug = new UGCourse("B.Sc Computer Science", 3);
         Course pg = new PGCourse("M.Sc Artificial Intelligence", 2);
+        Course diploma = new DiplomaCourse("Diploma in Data Science", 1);
 
         ug.displayDetails();
         System.out.println();
+
         pg.displayDetails();
+        System.out.println();
+
+        diploma.displayDetails();  
         System.out.println();
     }
 }
+
 
